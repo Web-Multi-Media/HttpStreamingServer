9c00ffd191780bfb8775c3de9bb48757c25a9bbf
Nicolas DEROUINEAU <nicodero72@gmail.com>
2018-11-25 13:49:18 +0100

    Add Recursive parsing of base folder

--- StreamServerApp/management/commands/populatedb.py
+++ StreamServerApp/management/commands/populatedb.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from os import listdir
+import os
 from os.path import isfile, join
 from StreamServerApp.models import Video
 from django.conf import settings
@@ -7,13 +7,20 @@ from django.conf import settings
 
 VIDEO_NAMES = ['canard', 'cochon', 'singe']
 
+
 class Command(BaseCommand):
     help = 'Populate video database'
 
     def handle(self, *args, **kwargs):
         Video.objects.all().delete()  # delete all videos in the db
         my_path = settings.SERVER_VIDEO_DIR
-        for idx, f in enumerate(listdir(my_path)):
-            if isfile(join(my_path, f)) and f.endswith(".mp4"):
-                v = Video(name=VIDEO_NAMES[idx % 3], baseurl=settings.REMOTE_BASE_URL + "/" + f)
-                v.save()
+        idx = 0
+        for root, directories, filenames in os.walk(my_path):
+            idx += len(filenames)
+            for filename in filenames:
+                full_path = os.path.join(root, filename)
+                relative_path = os.path.relpath(full_path, my_path)
+                if isfile(full_path) and full_path.endswith(".mp4"):
+                    v = Video(
+                        name=VIDEO_NAMES[idx % 3], baseurl=settings.REMOTE_BASE_URL + "/" + relative_path)
+                    v.save()
