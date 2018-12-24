af95cade51e1f63376f63f4cf653174d2afcb7f0
Nicolas DEROUINEAU <nicodero72@gmail.com>
2018-12-24 16:42:02 +0100

    Add longer strings in model

--- StreamServerApp/models.py
+++ StreamServerApp/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Video(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
     video_codec = models.CharField(max_length=100, default="")
     height = models.IntegerField(default=0)
     width = models.IntegerField(default=0)
     audio_codec = models.CharField(max_length=100, default="")
     metadata = models.CharField(max_length=100, default="")
-    baseurl = models.CharField(max_length=100, default="")
+    baseurl = models.CharField(max_length=300, default="")
