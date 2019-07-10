3baefd06e7887b45890773c497abea6a3244f51c
Nicolas DEROUINEAU <nicodero72@gmail.com>
2019-07-10 12:54:15 +0200

    Fix Api and Title

--- frontend/src/api/djangoAPI.js
+++ frontend/src/api/djangoAPI.js
@@ -2,6 +2,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:1337/StreamServerApp/',
-    //baseURL: '/StreamServerApp/',
+    //baseURL: 'http://localhost:1337/StreamServerApp/',
+    baseURL: '/StreamServerApp/',
 })
