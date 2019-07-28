5d04e4a0bdab6339e4fc50dddcd217eb4359ba70
Nicolas D <nicodero72@gmail.com>
2019-07-28 21:28:45 +0200

    Fix bug in admin action

--- backend/StreamServerApp/admin.py
+++ backend/StreamServerApp/admin.py
@@ -5,15 +5,9 @@ from django.conf import settings
 
 
 def reload_video(modeladmin, request, queryset):
-    selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
-    data = request.POST.copy()
-    if len(selected) == 0 and data['action'] in ('reload_video'):
-        delete_DB_Infos()
-        populate_db_from_local_folder(settings.VIDEO_ROOT, settings.VIDEO_URL)
-        reload_video.short_description = "Reload videos"
-    else:
-        return super(VideoAdmin, self).response_action(request, queryset)
-
+    delete_DB_Infos()
+    populate_db_from_local_folder(settings.VIDEO_ROOT, settings.VIDEO_URL)
+    reload_video.short_description = "Reload videos"
 
 class VideoAdmin(admin.ModelAdmin):
     actions = []
