72fb930c17c9ea23c0b615e235a79c42c2bd2512
Nicolas D <nicodero72@gmail.com>
2019-08-25 17:02:46 +0200

    Code cleaning

--- backend/StreamServerApp/subtitles.py
+++ backend/StreamServerApp/subtitles.py
@@ -10,21 +10,26 @@ from StreamingServer.settings import customstderr, customstdout
 
 
 def init_cache():
-    if(os.path.isfile('cachefile.dbm.db')==False):
+    """ # init cache for subtitles database query and stuff.
+    """
+    if(os.path.isfile('cachefile.dbm.db') == False):
         print("Create subtitles cache data")
         region.configure('dogpile.cache.dbm', arguments={
-                        'filename': 'cachefile.dbm'}, replace_existing_backend=True)
+            'filename': 'cachefile.dbm'}, replace_existing_backend=True)
 
 
 def get_subtitles(video_path):
+    """ # get subtitles and convert them to web vtt
+        Args:
+        video_path: absolute path to videos
+        return: empty string if no subtitles was found. Otherwise return subtitle absolute location
+    """
 
     webvtt_fullpath = ''
 
     video = Video.fromname(video_path)
 
     best_subtitles = download_best_subtitles([video], {Language('fra')})
-
-    print(best_subtitles[video])
 
     if(best_subtitles[video]):
         best_subtitle = best_subtitles[video][0]
@@ -35,7 +40,8 @@ def get_subtitles(video_path):
                 video_path, Language('fra'))
             webvtt_fullpath = os.path.splitext(srt_fullpath)[0]+'.vtt'
             try:
-                subprocess.run(["ffmpeg", "-n","-i", srt_fullpath, webvtt_fullpath])
+                subprocess.run(
+                    ["ffmpeg", "-n", "-i", srt_fullpath, webvtt_fullpath])
                 return webvtt_fullpath
             except subprocess.CalledProcessError as e:
                 print(e.returncode)
