99b8e147c90f8b0a6b3c4d733d5a009e7e7329a5
nicolasDEROUINEAU <nicodero72@gmail.com>
2019-12-30 17:23:30 +0100

    Add primary key field in serializer

--- backend/StreamServerApp/serializers.py
+++ backend/StreamServerApp/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class VideoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Video
-        fields = ['name', 'video_url', 'thumbnail', 'fr_subtitle_url', 'en_subtitle_url', 'ov_subtitle_url']
+        fields = ['id', 'name', 'video_url', 'thumbnail', 'fr_subtitle_url', 'en_subtitle_url', 'ov_subtitle_url']
