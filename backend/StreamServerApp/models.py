374f891ee5b2e9ac69626c7994824843425157b1
xavierfav <xavier.favory@gmail.com>
2019-12-31 19:25:07 +0100

    Add Video & Movie models

--- backend/StreamServerApp/models.py
+++ backend/StreamServerApp/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class Series(models.Model):
+    title = models.CharField(max_length=200)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+
+
 class Video(models.Model):
     name = models.CharField(max_length=200)
     video_codec = models.CharField(max_length=100, default="")
@@ -13,5 +21,14 @@ class Video(models.Model):
     thumbnail = models.CharField(max_length=300, default="")
     fr_subtitle_url = models.CharField(max_length=300, default="")
     en_subtitle_url = models.CharField(max_length=300, default="")
-    #This field describes the path to the subtitle shipped with the input video
+    # This field describes the path to the subtitle shipped with the input video
     ov_subtitle_url = models.CharField(max_length=300, default="")
+
+    # Relations to series and movies
+    # on_delete=SET_NULL keeps videos indexed if we remove a serie or a video it belongs to
+    series = models.ForeignKey(Series, null=True, on_delete=models.SET_NULL)
+    movie = models.ForeignKey(Movie, null=True, on_delete=models.SET_NULL)
+
+    # For series & movie episodes and series seasons
+    episode = models.PositiveSmallIntegerField(default=None, null=True)
+    season = models.PositiveSmallIntegerField(default=None, null=True)
