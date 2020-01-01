39930dd437b70607ad636c22917b69d4b414252e
xavierfav <xavier.favory@gmail.com>
2020-01-01 15:56:32 +0100

    Improve parsing video info test

--- backend/StreamServerApp/tests.py
+++ backend/StreamServerApp/tests.py
@@ -37,14 +37,25 @@ class LoadingTest(TestCase):
 class UtilsTest(TestCase):
     def test_movie_series_info_parsing(self):
         series_name = 'The.Big.Bang.Theory.S05E19.HDTV.x264-LOL.mp4'
+        series_name2 = 'The.Big.Bang.Theory.S05E18.HDTV.x264-LOL.mp4'
+        series_name3 = 'Futurama S02E10 Put Your Head On My Shoulders.mp4'
+        series_name4 = 'Futurama.S01.E10.Fake_name*withçweird&cg!"%&/()@.mp4'
         movie_name = 'The Blues Brothers (1980) [1080p]/The.Blues.Brothers.1980.1080p.BrRip.x264.bitloks.YIFY.jpg'
         series_info = get_video_type_and_info(series_name)
+        series_info2 = get_video_type_and_info(series_name2)
+        series_info3 = get_video_type_and_info(series_name3)
+        series_info4 = get_video_type_and_info(series_name4)
         movie_info = get_video_type_and_info(movie_name)
 
         self.assertEqual(series_info['type'], 'Series')
         self.assertEqual(series_info['title'], 'The Big Bang Theory')
         self.assertEqual(series_info['season'], 5)
         self.assertEqual(series_info['episode'], 19)
+
+        self.assertEqual(series_info2['season'], 5)
+        self.assertEqual(series_info3['title'], series_info4['title'])
+        self.assertEqual(series_info3['season'], 2)
+        self.assertEqual(series_info4['season'], 1)
 
         self.assertEqual(movie_info['type'], 'Movie')
         self.assertEqual(movie_info['title'], 'The Blues Brothers')
