cbfad98d3df1e6c7ad155a851920f0316a8da034
nicolasDEROUINEAU <nicodero72@gmail.com>
2020-01-07 12:17:26 +0100

    Adapt PR#24 with Django REST server response

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -54,9 +54,9 @@ class App extends React.Component {
 
     handleVideoSelect = (video) => {
         this.setState({ selectedVideo: video });
-        this.props.history.push("/streaming/?video=" + video.pk);
+        this.props.history.push("/streaming/?video=" + video.id);
         // change tab title with the name of the selected video
-        document.title = video.fields.name;
+        document.title = video.name;
         window.scrollTo(0, 0);
     };
 
