d79d47890b4c4b7c5cf09742d35258e5c1981dc6
Valentin <valentin.nerin@gmail.com>
2020-01-07 15:32:44 +0100

    Change data access

--- frontend/src/components/VideoDetail.js
+++ frontend/src/components/VideoDetail.js
@@ -10,10 +10,10 @@ const VideoDetail = ({ video }) => {
         <div>
             <div className='ui embed'>
                 <video preload="auto" controls width="320" height="240" key={video.id}>
-                    <source src={video.video_url} title='Video player' />
-                    {video.fr_subtitle_url && <track label="French" kind="subtitles" srcLang="fr" src={video.fr_subtitle_url} />}
-                    {video.en_subtitle_url && <track label="English" kind="subtitles" srcLang="eng" src={video.en_subtitle_url} />}
-                    {video.ov_subtitle_url && <track label="OV" kind="subtitles" srcLang="ov" src={video.ov_subtitle_url} />}
+                    <source src={video.videoUrl} title='Video player' />
+                    {video.fr_subtitle_url && <track label="French" kind="subtitles" srcLang="fr" src={video.frSubtitleUrl} />}
+                    {video.en_subtitle_url && <track label="English" kind="subtitles" srcLang="eng" src={video.enSubtitleUrl} />}
+                    {video.ov_subtitle_url && <track label="OV" kind="subtitles" srcLang="ov" src={video.ovSubtitleUrl} />}
                 </video>
             </div>
             <div className='ui segment'>
