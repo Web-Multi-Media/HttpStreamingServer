b1d11b66b2f5d950b8506d62f5522f692639c375
Valentin <valentin.nerin@gmail.com>
2020-01-08 08:30:58 +0100

    Switch to const instead of func & refactor

--- frontend/src/api/djangoAPI.js
+++ frontend/src/api/djangoAPI.js
@@ -14,31 +14,31 @@ const VIDEOS_ENDPOINT = '/videos';
  **************** VIDEOS *******************
  *******************************************
  */
-
-/**
- * performs GET request to retrieve a single video by it's ID
- *
- * @param id
- *          video's id
- * @returns {Promise<void>}
- *          videos list
- */
-function getVideoById(id) {
-    return http.get(`${VIDEOS_ENDPOINT}/${id}`, {transformResponse: [response => singleVideoMapper(response)]});
-}
-
-/**
- * performs GET request to retrieve videos list from searchbar entry
- * the param is optional, retrieve full video list instead if not provided
- * 
- * @param name 
- *          searchbar query, optional
- * @returns {Promise<void>}
- *          videos list
- */
-function searchVideos(searchQuery) {
-    const params = searchQuery ? {search_query: searchQuery} : null;
-    return http.get(`${VIDEOS_ENDPOINT}/`, {params: params, transformResponse: [response => videosMapper(response)]});
+const client = {
+    /**
+     * performs GET request to retrieve a single video by it's ID
+     *
+     * @param id
+     *          video's id
+     * @returns {Promise<void>}
+     *          videos list
+     */
+    getVideoById: id => {
+        return http.get(`${VIDEOS_ENDPOINT}/${id}`, { transformResponse: [response => mapper.video(response)] });
+    },
+    /**
+     * performs GET request to retrieve videos list from searchbar entry
+     * the param is optional, retrieve full video list instead if not provided
+     * 
+     * @param name 
+     *          searchbar query, optional
+     * @returns {Promise<void>}
+     *          videos list
+     */
+    searchVideos: searchQuery => {
+        const params = searchQuery ? { search_query: searchQuery } : null;
+        return http.get(`${VIDEOS_ENDPOINT}/`, { params: params, transformResponse: [response => mapper.videos(response)] });
+    }
 }
 
 /** 
@@ -46,68 +46,67 @@ function searchVideos(searchQuery) {
  **************** PAGER *******************
  ******************************************
  */
-
-/**
- * performs GET request to retrieve the next videos in the list
- *
- * @param nextQuery
- *          the full query, usually : "/streaming/videos/?limit=xx&offset=xx"
- * @returns {Promise<void>}
- *          videos list
- */
-function pagerNext(url) {
-    return http.get(url, {transformResponse: [response => videosMapper(response)]});
+const pager = {
+    /**
+     * performs GET request to retrieve the next videos in the list
+     *
+     * @param nextQuery
+     *          the full query, usually : "/streaming/videos/?limit=xx&offset=xx"
+     * @returns {Promise<void>}
+     *          videos list
+     */
+    nextPage: url => {
+        return http.get(url, { transformResponse: [response => mapper.videos(response)] });
+    }
 }
-
 
 /** 
  ******************************************
  **************** MAPPERS *****************
  ******************************************
  */
+const mapper = {
+    /**
+     * Create video standard object from response
+     * 
+     * @param response
+     *          expected response from api
+     * @returns
+     *          mapped object video
+     */
+    video: response => {
+        return {
+            id: response.id,
+            name: response.name,
+            videoUrl: response.video_url,
+            thumbnail: response.thumbnail,
+            frSubtitleUrl: response.fr_subtitle_url,
+            enSubtitleUrl: response.en_subtitle_url,
+            ovSubtitleUrl: response.ov_subtitle_url
+        }
+    },
+    /**
+     * Create videos list standard object from response 
+     *
+     * @param response
+     *          expected response from api
+     * @returns
+     *          mapped object videos list
+     */
+    videos: response => {
+        const numberOfPages = Math.ceil(response.count / response.results.length);
+        const videosPerPages = response.results.length;
+        // use the mapper video for every videos
+        const videos = response.results.map(video => mapper.video(video));
 
-/**
- * Map videos list from response into standard object
- *
- * @param response
- *          expected response from api
- * @returns
- *          mapped object videos list
- */
-function videosMapper(response) {
-    const numberOfPages = Math.ceil(response.count / response.results.length);
-    const videosPerPages = response.results.length;
-    // use the singleVideoMapper for every videos
-    const videos = response.results.map(video => singleVideoMapper(video));
-
-    return {
-        count: response.count,
-        videos: videos,
-        numberOfPages: numberOfPages,
-        videosPerPages: videosPerPages, 
-        nextQuery: response.next,
-        previousQuery: response.previous
-    }
-}
-
-/**
- * Map single video from response into standard object
- *
- * @param response
- *          expected response from api
- * @returns
- *          mapped object video
- */
-function singleVideoMapper(response) {
-
-    return {
-        id: response.id,
-        name: response.name,
-        videoUrl: response.video_url,
-        thumbnail: response.thumbnail,
-        frSubtitleUrl: response.fr_subtitle_url,
-        enSubtitleUrl: response.en_subtitle_url,
-        ovSubtitleUrl: response.ov_subtitle_url
+        return {
+            count: response.count,
+            videos: videos,
+            numberOfPages: numberOfPages,
+            videosPerPages: videosPerPages,
+            nextQuery: response.next,
+            previousQuery: response.previous
+        }
     }
 }
 
@@ -133,4 +132,4 @@ function handleError(err) {
     }
 }
 
-export {getVideoById, searchVideos, pagerNext, handleError}
+export {client, pager, handleError}
