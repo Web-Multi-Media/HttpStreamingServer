e41991badad224c2a874ec6363bf4e24899226bd
Nicolas DEROUINEAU <nicodero72@gmail.com>
2020-01-19 12:12:35 +0100

    Fix subtitles in frontend

--- frontend/src/components/VideoDetail.js
+++ frontend/src/components/VideoDetail.js
@@ -11,9 +11,9 @@ const VideoDetail = ({ video }) => {
             <div className='ui embed'>
                 <video preload="auto" controls width="320" height="240" key={video.id}>
                     <source src={video.videoUrl} title='Video player' />
-                    {video.fr_subtitle_url && <track label="French" kind="subtitles" srcLang="fr" src={video.frSubtitleUrl} />}
-                    {video.en_subtitle_url && <track label="English" kind="subtitles" srcLang="eng" src={video.enSubtitleUrl} />}
-                    {video.ov_subtitle_url && <track label="OV" kind="subtitles" srcLang="ov" src={video.ovSubtitleUrl} />}
+                    {video.frSubtitleUrl && <track label="French" kind="subtitles" srcLang="fr" src={video.frSubtitleUrl} />}
+                    {video.enSubtitleUrl && <track label="English" kind="subtitles" srcLang="eng" src={video.enSubtitleUrl} />}
+                    {video.ovSubtitleUrl && <track label="OV" kind="subtitles" srcLang="ov" src={video.ovSubtitleUrl} />}
                 </video>
             </div>
             <div className='ui segment'>
