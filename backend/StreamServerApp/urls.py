d30f50f62df085aa7504c51d8c1171e826eeb759
Nicolas DEROUINEAU <nicodero72@gmail.com>
2020-02-03 20:05:58 +0100

    Add serializer for movie

--- backend/StreamServerApp/urls.py
+++ backend/StreamServerApp/urls.py
@@ -9,7 +9,7 @@ from . import views
 router = DefaultRouter()
 router.register(r'videos', views.VideoViewSet, basename='videos')
 router.register(r'series', views.SeriesViewSet, basename='series')
-#router.register(r'movies', views.MoviesViewSet, basename='movie')
+router.register(r'movies', views.MoviesViewSet, basename='movie')
 
 
 urlpatterns = [
