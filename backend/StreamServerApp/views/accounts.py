772e9b3e4984fb17a295c257c568177bf0829c8c
xavierfav <xavier.favory@gmail.com>
2020-05-27 18:04:50 +0200

    Fix post history

--- backend/StreamServerApp/views/accounts.py
+++ backend/StreamServerApp/views/accounts.py
@@ -2,38 +2,18 @@ from django.conf import settings
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import action
 from django.contrib.auth.models import User
 
 from StreamServerApp.serializers.videos import ExtendedVideoSerializer
 from StreamServerApp.models import Video, UserVideoHistory
 
 
-# class HistoryViewSet(viewsets.ModelViewSet):
-#     """
-#     This viewset automatically provides 
-#     """
-
-#     permission_classes = (IsAuthenticated,)
-
-#     queryset = Video.objects.all()
-#     serializer_class = ExtendedVideoSerializer
-
-#     def _allowed_methods(self):
-#         return ['GET', 'POST']
-
-
 class History(APIView):
     """
     Get, create and update user history
     """
-
-    permission_classes = [IsAuthenticated]
-
     def get(self, request):
-        print(request.user)
-        user_token = request.data.get('token')
+        user_token = request.data.get('headers').get('Authorization')
         user = User.objects.get(auth_token=user_token)
 
         queryset = Video.objects.filter(history__user=user).order_by('-updated_at')
@@ -41,10 +21,10 @@ class History(APIView):
         return Response(serializer.data)
         
     def post(self, request):
-        user_token = request.data.get('token')
-        video_id = request.data.get('video-id')
-        time = request.data.get('video-time', 0)
-
+        user_token = request.data.get('headers').get('Authorization')
+        video_id = request.data.get('body').get('video-id')
+        time = request.data.get('body').get('video-time', 0)
+        print(user_token)
         user = User.objects.get(auth_token=user_token)
         video = Video.objects.get(id=video_id)
 
