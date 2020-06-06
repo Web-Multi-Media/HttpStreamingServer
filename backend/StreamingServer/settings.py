3ab5261bbf65e3ab97789e8282baf4f71fafe7e8
DerouineauNicolas <nicodero72@gmail.com>
2020-06-06 16:55:48 +0200

    Uncomment STATICFILES_DIRS in settings.py

--- backend/StreamingServer/settings.py
+++ backend/StreamingServer/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, '../frontend/build/static/'),  # update the STATICFILES_DIRS
-# )
+STATICFILES_DIRS = (
+     os.path.join(BASE_DIR, '../frontend/build/static/'),  # update the STATICFILES_DIRS
+)
 
 ALLOWED_HOSTS = ['web', 'localhost']
 
