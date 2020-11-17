3c834b061ae3e4db0c9069d296586a72c16fb977
DerouineauNicolas <nicodero72@gmail.com>
2020-11-17 17:16:26 +0100

    Update admin.py

--- backend/StreamServerApp/admin.py
+++ backend/StreamServerApp/admin.py
@@ -4,24 +4,7 @@ from StreamServerApp.database_utils import delete_DB_Infos, populate_db_from_loc
 from django.conf import settings
 
 
-def populate_videos(modeladmin, request, queryset):
-    delete_DB_Infos()
-    populate_db_from_local_folder(settings.VIDEO_ROOT, settings.VIDEO_URL)
-    populate_videos.short_description = "Populate videos database"
-
-
-def update_videos(modeladmin, request, queryset):
-    update_db_from_local_folder(settings.VIDEO_ROOT, settings.VIDEO_URL)
-    update_videos.short_description = "Update videos database"
-
-
-class VideoAdmin(admin.ModelAdmin):
-    actions = []
-
-
-admin.site.register(Video, VideoAdmin)
-admin.site.register(Movie, VideoAdmin)
-admin.site.register(Series, VideoAdmin)
-admin.site.register(Subtitle, VideoAdmin)
-admin.site.add_action(populate_videos)
-admin.site.add_action(update_videos)
+admin.site.register(Video)
+admin.site.register(Movie)
+admin.site.register(Series)
+admin.site.register(Subtitle)
