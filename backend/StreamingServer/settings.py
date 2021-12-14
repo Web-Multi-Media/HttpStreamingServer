83f7daf816fca1301206efacc71802bcce5d0471
nicolasDEROUINEAU <nicodero72@gmail.com>
2021-12-14 11:20:11 +0100

    Remove backend code mapping in docker-compose prod and use different docker base image for ffmpeg

--- backend/StreamingServer/settings.py
+++ backend/StreamingServer/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/usr/static/'
 
 ALLOWED_HOSTS = ['web', 'localhost']
 
