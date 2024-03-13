85742ae52839aa7afc52df109c8d93d52b8a15ba
Nicolas DEROUINEAU <nicodero72@gmail.com>
2024-03-13 11:57:42 +0100

    switch videocarrouselslick to functionnal comp

--- frontend/src/components/Carousels/VideoCarrouselSlick.js
+++ frontend/src/components/Carousels/VideoCarrouselSlick.js
@@ -1,112 +1,83 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Slider from "react-slick";
 import SampleNextArrow from "./SampleNextArrow";
 import SamplePrevArrow from "./SamplePrevArrow";
 import '../../style/style.scss';
+import { useState, useEffect } from 'react';
 
 
-export default  class VideoCarrouselSlick extends Component {
 
-    //this variable must be the same as PAGE_SIZE in settings.py
-    SLIDES_OF_CAROUSEL = 5;
+export default function VideoCarrouselSlick({ pager, videos, handleVideoSelect, reset }) {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            pager: this.props.pager,
-            videos: this.props.videos
-        };
-        this.afterChangeMethod = this.afterChangeMethod.bind(this);
+    const [index, setIndex] = useState(0);
+    const SLIDES_OF_CAROUSEL = 5;
+
+    const chooseIndex = (reset) => {
+        if (reset === true) {
+            return index;
+        }
+        return 0;
     };
 
-
-
-    componentWillReceiveProps(nextProps) {
-        const chooseIndex = (reset) =>{
-            if(reset === true){
-               return this.state.index;
-            }
-            return 0;
-        };
-        if (nextProps.videos !== this.props.videos) {
-            const index = chooseIndex(nextProps.reset);
-            this.setState({
-                pager: nextProps.pager,
-                videos: nextProps.videos
-            }, () => this.slider.slickGoTo(index, false));
+    const setSeriePagerIndex = (index) => {
+        if (pager.type === 'Serie') {
+            setIndex(index);
         }
     };
 
+    const afterChangeMethod = async (index) => {
+         // Assuming this constant value, adjust as needed
 
-    /**
-     * this method is called by react slick after the slider finish transition
-     * used to compute if we need to make new API calls
-     * @param index
-     * @returns {Promise<void>}
-     */
-    async afterChangeMethod(index) {
-        const setSeriePagerIndex = (index) =>{
-            if(this.state.pager.type === 'Serie'){
-                this.setState({
-                    index: index
-                });
-            }
-        };
-        const isLastPage = (index + this.SLIDES_OF_CAROUSEL) === this.state.videos.length;
+        const isLastPage = (index + SLIDES_OF_CAROUSEL) === videos.length;
         setSeriePagerIndex(index);
-        if (isLastPage && this.state.pager.nextPageUrl){
+        
+        if (isLastPage && pager.nextPageUrl) {
             // API call to retrieve more videos when navigating through carousel
             try {
-                let pager = this.state.pager;
-                await pager.getNextPage();
-                let videos = this.state.videos;
-                videos.push(...pager.videos);
-                this.setState({
-                    pager: pager,
-                    videos: videos
-                });
-            } catch(error) {
+                let updatedPager = { ...pager };
+                await updatedPager.getNextPage();
+                let updatedVideos = [...videos, ...updatedPager.videos];
+                setIndex(index);
+            } catch (error) {
                 console.log(error);
             }
         }
     };
 
-    render() {
-        const settings = {
-            dots: false,
-            infinite: false,
-            speed: 500,
-            slidesToShow: this.SLIDES_OF_CAROUSEL,
-            slidesToScroll: this.SLIDES_OF_CAROUSEL,
-            nextArrow: <SampleNextArrow />,
-            prevArrow: <SamplePrevArrow />,
-            afterChange: current => this.afterChangeMethod(current)
-        };
+    useEffect(() => {
+        const index = chooseIndex(reset);
+        setIndex(index);
+    }, [reset]);
 
-        const slider = this.state.videos.map((video) => {
-            return <div className="video-element" key={video.id}>
-             <div className="video-element2"
-                  onClick={() => this.props.handleVideoSelect(video)}>
-                    <img
-                        className={`img-cover`}
-                        src={video.thumbnail}
-                    />
-                    <div className={`shadow-element`}>
-                        <p
-                            className={`paragraph-element`}
-                        >{video.name}</p>
-                    </div>
-                   </div>
-            </div>
-        });
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: SLIDES_OF_CAROUSEL,
+        slidesToScroll: 1,
+        afterChange: afterChangeMethod,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />,
+      };
 
-        return (
-            <div>
-                <Slider ref={c => (this.slider = c)} {...settings}>
-                    {slider}
-                </Slider>
-            </div>
-        );
-    }
-}
-
+    return (
+      <Slider {...settings}>
+            {videos.map((video) => (
+                        <div className="video-element" key={video.id}>
+                        <div className="video-element2"
+                            onClick={() => handleVideoSelect(video)}>
+                                <img
+                                    className={`img-cover`}
+                                    src={video.thumbnail}
+                                />
+                                <div className={`shadow-element`}>
+                                    <p
+                                        className={`paragraph-element`}
+                                    >{video.name}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+      </Slider>
+    );
+  }
