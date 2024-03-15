7d85a99d9baac050b6fee31b4ec4021ffc4b6354
Nicolas DEROUINEAU <nicodero72@gmail.com>
2024-03-15 14:56:53 +0100

    convert movies/series carrousels to functionnal components

---
+++ frontend/src/components/Carousels/SeriesCarousel.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import '../../style/style.scss';
+import VideoCarrouselSlick from "./VideoCarrouselSlick";
+import SelectBar from "./SelectBar";
+
+
+const SeriesCarousel = ({ pager, videos, handleVideoSelect }) => {
+
+    const [series, setSeries] = useState('');
+    const [seriesPager, setSeriesPager] = useState(pager);
+    const [seasons, setSeasons] = useState([]);
+    const [episode, setEpisode] = useState('');
+    const [seriesId, setSeriesId] = useState(0);
+    const [reset, setReset] = useState(false);
+    const [Seriesvideos, setVideos] = useState(videos);
+    const [refresh, setRefresh] = useState(false);
+
+
+    const getSeriesSeason = async (serie) => {
+        try {
+            await serie.getSeason();
+            await serie.getEpisodes(serie.seasons[0]);
+            setSeriesPager(serie);
+            setSeries(serie.name);
+            setSeasons(serie.seasons);
+            setSeriesId(serie);
+            setReset(false);
+            setVideos(serie.videos);
+
+        } catch(error) {
+            console.log(error);
+        }
+    };
+
+    const handleSeasonSelect = async (e) => {
+        const serie = seriesPager;
+        await serie.getEpisodes(e.target.value);
+        setSeriesPager(serie);
+        setVideos(serie.videos);
+        setReset(false);
+    };
+
+    const handleSeriesSelect = async (video) => {
+        if (series === '') {
+            await getSeriesSeason(video);
+            setRefresh(true);
+        }
+        else{
+            handleVideoSelect(video);
+            setEpisode(video.name);
+        }
+        // change tab title with the name of the selected video
+        document.title = video.name;
+    };
+
+    const resetSeries = () => {
+        setSeriesPager(pager);
+        setSeries('');
+        setSeasons([]);
+        setEpisode('')
+        setReset(true);
+        setVideos(videos);
+        setRefresh(true);
+    };
+
+
+        return (
+            <div>
+                <div className="seriesDisplay">
+                <h4 className="centerVer hover-hilight" onClick={()=>resetSeries()}>SERIES</h4>
+                {series.length > 0 &&
+                    <React.Fragment>
+                    <span className="centerVer"> > {series} > </span>
+                    <SelectBar
+                    seasons = {seasons}
+                    handleSeason= {handleSeasonSelect}
+                    />
+                    </React.Fragment>
+                }
+                {episode !== '' &&  <span className="centerVer"> > {episode}</span>}
+                </div>
+                {Seriesvideos.length > 0 &&
+                <div>
+                    <VideoCarrouselSlick
+                        pager={seriesPager}
+                        videos={Seriesvideos}
+                        handleVideoSelect={handleSeriesSelect}
+                        reset={reset}
+                        refresh={refresh}
+                        setRefresh={setRefresh}
+                    />
+                </div>
+                }
+            </div>
+        );
+}
+export default SeriesCarousel;
---
+++ frontend/src/components/Carousels/VideoCarrouselSlick.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import Slider from "react-slick";
+import SampleNextArrow from "./SampleNextArrow";
+import SamplePrevArrow from "./SamplePrevArrow";
+import '../../style/style.scss';
+import { useState, useEffect } from 'react';
+
+
+
+export default function VideoCarrouselSlick({ pager, videos, handleVideoSelect, reset, refresh, setRefresh}) {
+
+    const [index, setIndex] = useState(0);
+    const [carrouselVideos, setCarrouselVideos] = useState(videos);
+    const SLIDES_OF_CAROUSEL = 5;
+
+    const chooseIndex = (reset) => {
+        if (reset === true) {
+            return index;
+        }
+        return 0;
+    };
+
+    const setSeriePagerIndex = (index) => {
+        if (pager.type === 'Serie') {
+            setIndex(index);
+        }
+    };
+
+    const afterChangeMethod = async (index) => {
+         // Assuming this constant value, adjust as needed
+
+        const isLastPage = (index + SLIDES_OF_CAROUSEL) === videos.length;
+        setSeriePagerIndex(index);
+        
+        if (isLastPage && pager.nextPageUrl) {
+            // API call to retrieve more videos when navigating through carousel
+            try {
+                let updatedPager = pager ;
+                await updatedPager.getNextPage();
+                let updatedVideos = [...videos, ...pager.videos];
+                setCarrouselVideos(updatedVideos);
+                setIndex(index);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    };
+
+    useEffect(() => {
+        const index = chooseIndex(reset);
+        setIndex(index);
+    }, [reset]);
+
+    useEffect(() => {
+        if (refresh) {
+            setCarrouselVideos(videos)
+            setRefresh(false)
+
+        }
+    }, [refresh]);
+
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: SLIDES_OF_CAROUSEL,
+        slidesToScroll: 1,
+        afterChange: afterChangeMethod,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />,
+      };
+
+    return (
+      <Slider {...settings}>
+            {carrouselVideos.map((video) => (
+                        <div className="video-element" key={video.id}>
+                        <div className="video-element2"
+                            onClick={() => handleVideoSelect(video)}>
+                                <img
+                                    className={`img-cover`}
+                                    src={video.thumbnail}
+                                />
+                                <div className={`shadow-element`}>
+                                    <p
+                                        className={`paragraph-element`}
+                                    >{video.name}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+      </Slider>
+    );
+  }
